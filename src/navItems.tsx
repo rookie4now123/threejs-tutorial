1b29512426e268c8f78e21f35db8b614ccb5dedc
Julian <renren.cheung@gmail.com>
2025-08-13 16:50:49 +0200

    vercel.json added

--- src/navItems.tsx
+++ src/navItems.tsx
@@ -2,7 +2,7 @@ export const navItems = [
     { id: 'font', title: 'Introduction', path: '/' },
     { id: 'galaxy', title: 'Galaxy', path: '/galaxy' },
     { id: 'contact', title: 'Storm', path: '/scrollbase' },
-    { id: 'memorytest', title: 'Memorytest', path: 'memorytest' },
     { id: 'Weiredface', title: 'Weiredface', path: 'Weiredface' },
     { id: 'test3', title: 'Physics', path: '/test3' },
+    { id: 'memorytest', title: 'Memorytest', path: 'memorytest' },
   ];
